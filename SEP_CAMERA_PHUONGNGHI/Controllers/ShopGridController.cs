2541d3c5101903ec4f6de93da63b4c3a497bde20
Lê Trường Thịnh <81286592+maverik0302@users.noreply.github.com>
2022-07-04 13:39:15 +0700

    Add project files.

---
+++ SEP_CAMERA_PHUONGNGHI/Controllers/ShopGridController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SEP_CAMERA_PHUONGNGHI.Controllers
+{
+    public class ShopGridController : Controller
+    {
+        // GET: ShopGrid
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SEP_CAMERA_PHUONGNGHI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SEP_CAMERA_PHUONGNGHI.MvcApplication" Language="C#" %>
