e0097bdbe57ecef992facc78f2e54f02cabdae1b
Your Name <you@example.com>
2022-07-05 20:44:19 +0700

    Pay

---
+++ SEP_CAMERA_PHUONGNGHI/Controllers/PayController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SEP_CAMERA_PHUONGNGHI.Controllers
+{
+    public class PayController : Controller
+    {
+        // GET: Pay
+        public ActionResult Pay()
+        {
+            return View();
+        }
+    }
+}
