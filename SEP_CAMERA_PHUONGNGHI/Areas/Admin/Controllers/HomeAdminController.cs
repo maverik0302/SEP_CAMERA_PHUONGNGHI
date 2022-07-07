2a958ea350823717f1a9c04bf2a5626bea3f66ac
197PM21962 - NGUYỄN HIẾU NHÂN - K25T-PM1 <nhan.197pm21962@vanlanguni.vn>
2022-07-07 18:27:26 +0700

    Login/out - Cap nhat trang chu admin / Xac thuc tai khoan

--- SEP_CAMERA_PHUONGNGHI/Areas/Admin/Controllers/HomeAdminController.cs
+++ SEP_CAMERA_PHUONGNGHI/Areas/Admin/Controllers/HomeAdminController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SEP_CAMERA_PHUONGNGHI.Areas.Admin.Middleware;
 using System.Web.Mvc;
 
 namespace SEP_CAMERA_PHUONGNGHI.Areas.Admin.Controllers
 {
+    [LoginVerification]
     public class HomeAdminController : Controller
     {
         // GET: Admin/HomeAdmin
