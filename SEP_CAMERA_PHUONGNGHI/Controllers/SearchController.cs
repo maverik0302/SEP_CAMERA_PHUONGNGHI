1ce3b9c4a6d8c533fe95defb0f3ec76ac9ef0a12
Lê Trường Thịnh <81286592+maverik0302@users.noreply.github.com>
2022-07-16 15:05:41 +0700

    Timkiemsp

---
+++ SEP_CAMERA_PHUONGNGHI/Controllers/SearchController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SEP_CAMERA_PHUONGNGHI.Models;
+using Pagedlist;
+
+namespace SEP_CAMERA_PHUONGNGHI.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
