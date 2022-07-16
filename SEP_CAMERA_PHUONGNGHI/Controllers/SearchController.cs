9e155b17c01c0a7de8f86070ae703225a83cd0ae
Lê Trường Thịnh <81286592+maverik0302@users.noreply.github.com>
2022-07-16 15:24:03 +0700

    Add lib

--- SEP_CAMERA_PHUONGNGHI/Controllers/SearchController.cs
+++ SEP_CAMERA_PHUONGNGHI/Controllers/SearchController.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SEP_CAMERA_PHUONGNGHI.Models;
-using Pagedlist;
+using PagedList;
+using PagedList.Mvc;
 
 namespace SEP_CAMERA_PHUONGNGHI.Controllers
 {
