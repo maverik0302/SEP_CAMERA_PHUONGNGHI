44353e375aa0eba97789f831aa5ca2256124dde5
Lê Trường Thịnh <81286592+maverik0302@users.noreply.github.com>
2022-07-18 19:21:37 +0700

    Update 7PM

--- SEP_CAMERA_PHUONGNGHI/Controllers/SearchController.cs
+++ SEP_CAMERA_PHUONGNGHI/Controllers/SearchController.cs
@@ -12,9 +12,23 @@ namespace SEP_CAMERA_PHUONGNGHI.Controllers
     public class SearchController : Controller
     {
         // GET: Search
-        public ActionResult Index()
+        SEP25Team01Entities db = new SEP25Team01Entities();
+        [HttpPost]
+        public ActionResult KetQuaTimKiem(FormCollection f, int? page)
         {
-            return View();
+            string sKeyword = f["txtSearch"].ToString();
+            List<tbProduct> ketQuaTK = db.tbProducts.Where(x => x.Name.Contains(sKeyword)).ToList();
+            //phantrang
+            int pageNumber = (page ?? 1);
+            int pageSize = 9;
+            if (ketQuaTK.Count == 0)
+            {
+                ViewBag.Thongbao = "Không tìm thấy sản phẩm nào";
+                return View(db.tbProducts.OrderBy(x => x.Name).ToPagedList(pageNumber, pageSize));
+            }
+            ViewBag.Thongbao = "Đã tìm thấy" + ketQuaTK.Count + "Kết quả";
+            
+            return View(ketQuaTK.OrderBy(x=>x.Name).ToPagedList(pageNumber, pageSize));
         }
     }
 }
