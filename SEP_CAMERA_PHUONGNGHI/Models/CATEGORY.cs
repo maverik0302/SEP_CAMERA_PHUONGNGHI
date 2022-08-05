8b496cac9667971f717c33d98b69f6c436dd741e
197PM21962 - NGUYỄN HIẾU NHÂN - K25T-PM1 <nhan.197pm21962@vanlanguni.vn>
2022-08-05 13:03:08 +0700

    Cap nhat rang buoc chuc nang quan li bai viet, hang san pham, danh muc, tai khoan

--- SEP_CAMERA_PHUONGNGHI/Models/CATEGORY.cs
+++ SEP_CAMERA_PHUONGNGHI/Models/CATEGORY.cs
@@ -21,9 +21,11 @@ namespace SEP_CAMERA_PHUONGNGHI.Models
         {
             this.tbProducts = new HashSet<tbProduct>();
         }
-    
+
         public int category_id { get; set; }
+        [StringLength(250, MinimumLength = 1)]
         public string Name { get; set; }
+        [StringLength(250, MinimumLength = 1)]
         public string SeoTitle { get; set; }
         public Nullable<bool> Status { get; set; }
         public Nullable<int> sort { get; set; }
