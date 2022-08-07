bb913b91cb63037f8fa158a0c8f89be50fff2e58
197PM21962 - NGUYỄN HIẾU NHÂN - K25T-PM1 <nhan.197pm21962@vanlanguni.vn>
2022-08-07 14:47:13 +0700

    Cap nhat chuc nang bai gioi thieu

---
+++ SEP_CAMERA_PHUONGNGHI/Areas/Admin/Controllers/AboutController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SEP_CAMERA_PHUONGNGHI.Models;
+
+namespace SEP_CAMERA_PHUONGNGHI.Areas.Admin.Controllers
+{
+    public class AboutController : Controller
+    {
+        private SEP25Team01Entities db = new SEP25Team01Entities();
+
+        // GET: Admin/About
+        public ActionResult Index()
+        {
+            var model = db.Abouts.ToList();
+            return View(model);
+        }
+
+        // GET: Admin/About/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            About about = db.Abouts.Find(id);
+            if (about == null)
+            {
+                return HttpNotFound();
+            }
+            return View(about);
+        }
+
+        // GET: Admin/About/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/About/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "about_id,Title,Description,Owner,MetaKey,MetaDescription,CreateDate,UpdateDate")] About about)
+        {
+            if (ModelState.IsValid)
+            {
+                about.CreateDate = DateTime.Now;
+                db.Abouts.Add(about);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(about);
+        }
+
+        // GET: Admin/About/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            About about = db.Abouts.Find(id);
+            if (about == null)
+            {
+                return HttpNotFound();
+            }
+            return View(about);
+        }
+
+        // POST: Admin/About/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "about_id,Title,Description,Owner,MetaKey,MetaDescription,CreateDate,UpdateDate")] About about)
+        {
+            if (ModelState.IsValid)
+            {
+                about.UpdateDate = DateTime.Now;
+                db.Entry(about).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(about);
+        }
+
+        // GET: Admin/About/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            About about = db.Abouts.Find(id);
+            if (about == null)
+            {
+                return HttpNotFound();
+            }
+            return View(about);
+        }
+
+        // POST: Admin/About/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            About about = db.Abouts.Find(id);
+            db.Abouts.Remove(about);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
