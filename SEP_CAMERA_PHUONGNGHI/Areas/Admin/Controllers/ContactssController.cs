339848804826325b8d7d20df4bfb079cbab1e124
197PM21962 - NGUYỄN HIẾU NHÂN - K25T-PM1 <nhan.197pm21962@vanlanguni.vn>
2022-08-25 01:09:25 +0700

    Cap nhat chuc nang lien he cua khach hang

---
+++ SEP_CAMERA_PHUONGNGHI/Areas/Admin/Controllers/ContactssController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SEP_CAMERA_PHUONGNGHI.Models;
+
+namespace SEP_CAMERA_PHUONGNGHI.Areas.Admin.Controllers
+{
+    public class ContactssController : Controller
+    {
+        private SEP25Team01Entities db = new SEP25Team01Entities();
+
+        // GET: Admin/Contactss
+        public ActionResult Index()
+        {
+            return View(db.Contacts.ToList());
+        }
+
+        // GET: Admin/Contactss/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+        // GET: Admin/Contactss/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: Admin/Contactss/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            db.Contacts.Remove(contact);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
