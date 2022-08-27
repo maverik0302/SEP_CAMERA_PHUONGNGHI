6061850bd1344a5c075e3ef8b951e71a476c0d17
197PM21962 - NGUYỄN HIẾU NHÂN - K25T-PM1 <nhan.197pm21962@vanlanguni.vn>
2022-08-28 00:29:37 +0700

    Cap nhat giao dien

--- SEP_CAMERA_PHUONGNGHI/Controllers/GioHangController.cs
+++ SEP_CAMERA_PHUONGNGHI/Controllers/GioHangController.cs
@@ -176,7 +176,6 @@ namespace SEP_CAMERA_PHUONGNGHI.Controllers
         {
             if (Session["Account"] == null || Session["Account"].ToString() == "")
             {
-                ViewBag.Message = "Vui lòng đăng nhập";
                 return RedirectToAction("Login", "Auth", new { area = "Admin" });
             }
             if (Session["GioHang"] == null)
